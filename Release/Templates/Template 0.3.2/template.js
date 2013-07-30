7ce178f064e45f73fc987257e955618a6ffd9fbf
Zenka <w.r.vanvliet@gmail.com>
2013-07-30 11:50:42 +0200

    -Release Template 0.3.2 -GitIgnore some metadata stuff

---
+++ Release/Templates/Template 0.3.2/template.js
@@ -0,0 +1,30 @@
+﻿//test.js. We do stuff external, to keep it tidy.
+var context;
+var canvas;
+
+
+//this function will be called every time a new slide is called
+function newSlideHdl(index, backwards){
+	console.log("newSlideHdl called " + index + " backwards: " + backwards);	
+	if(index==1){
+		//jQuery.Edge.getComposition( "starcrafts" ).getStage().stop(0);
+	}
+}
+
+//this function will be called every time a new slide has stopped moving and is now standing in it's new position
+function newSlideStopHdl(index, backwards){
+	console.log("newSlideStopHdlcalled " + index + " backwards: " + backwards);
+	if(index==1){
+		//jQuery.Edge.getComposition( "starcrafts" ).getStage().play(0);
+	}
+}
+		
+function onLoad(){	
+	console.log("------------onLoad------------");
+	$(function() {
+    	$(".ph_Hatching_red").draggable();
+    	$(".ph_Hatching_green").draggable();
+    	$(".ph_Hatching_blue").draggable();
+    	console.log("made draggable");
+    });
+}
