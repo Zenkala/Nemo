aa76bcc0b323c4b5f862a817d0737d688a74193f
Zenka <w.r.vanvliet@gmail.com>
2013-09-10 11:59:41 +0200

    -Added NM_Box Component (Object) -Slight alteration NM_Text icon

---
+++ Adobe Dreamweaver CS6/configuration/Objects/Nemo/NM_Box.js
@@ -0,0 +1,17 @@
+// Copyright 2000, 2001, 2002, 2003, 2004, 2005 Macromedia, Inc. All rights reserved.
+
+//---------------     API FUNCTIONS    ---------------
+
+function isDOMRequired() {
+	// Return false, indicating that this object is available in code view.
+	return true;
+}
+
+function isAsset() {
+	return true;
+}
+
+function objectTag(assetArgs) {
+  return '<div class="nm_Box" style="position: absolute; left: 100px; top:300px; width: 100px; height: 100px;"></div>';
+}
+
